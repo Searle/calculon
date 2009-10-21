10fac3d345b85db95332a8944832ce21841b12e3
dr-berlin <info1@raisin.de>
2009-10-21 20:41:53 +0200

    Add initial files

---
+++ assets/calculon-data.js
@@ -0,0 +1,21 @@
+var doc= {
+
+    sheets: [
+        {
+            title: '0908',
+            data: {
+                100000: { value: '\'Jana', },
+                100001: { value: 'foo=ARRAY( B2:C2 ,A2:A3, zuppi:zappi)', },
+                200001: { value: 'bar1=\'HUHU', },
+                300001: { value: 'bar2 = \'HAHA', },
+                100002: { value: '123 +-123--zuppi()+zap', },
+                200002: { value: 224.1, },
+                300002: { value: 325.5e30, },
+                100003: { value: '+124', },
+                200003: { value: -223e23, },
+                300003: { value: 323, },
+            },
+        }
+    ],
+
+};
