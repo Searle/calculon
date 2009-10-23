bdf01374608bcd78292a39511394df4c8eafe484
dr-berlin <info1@raisin.de>
2009-10-23 23:12:40 +0200

    Semikolons entfernt, ist schliesslich JS 1.7

--- assets/ranges3.js
+++ assets/ranges3.js
@@ -1,7 +1,26 @@
 
 (function () {
 
+// TODO: Auf 3D (sheets) ausbauen
+
+// =============================================================================
+//      Utils
+// =============================================================================
+
         var undefined;
+
+        var _isFunction= function( obj ) {
+            return Object.prototype.toString.call(obj) === "[object Function]"
+        }
+
+        var _isArray= function( obj ) {
+            return Object.prototype.toString.call(obj) === "[object Array]"
+        };
+
+
+// =============================================================================
+//      Cell
+// =============================================================================
 
         var cells= {};
 
@@ -31,14 +50,7 @@
             return _getCell(atomId, x, y);
         };
 
-        var _isFunction= function( obj ) {
-            return Object.prototype.toString.call(obj) === "[object Function]"
-        }
-
-        var _isArray= function( obj ) {
-            return Object.prototype.toString.call(obj) === "[object Array]"
-        };
-
+        // FIXME: Naive Implementierung, kann kein AA1 etc
         var stringToCell= function( str ) {
             var x= "ABCDEFGHIJKLMNOPQRSTUVWXYZ".indexOf(str.substr(0, 1));
             if ( x < 0 ) throw "NoValidCellName";
@@ -47,12 +59,20 @@
             return [ x, y - 1 ];
         };
 
-        var _atomId= 0;
+
+// =============================================================================
+//      Atom
+// =============================================================================
+
+        var _atomId= 0
+
+        var atoms= {}
 
         var _Atom= function( parent ) {
 
-            this._atomId= _atomId++;
+            this._atomId= _atomId++
 
+            // Diese Methoden koennen nicht in den prototype, da es Closures sind
             this.recalcDeps= function() parent ? parent.recalcDeps() : []
             this.valueDeps= function()  parent ? parent.valueDeps()  : []
         }
@@ -62,26 +82,26 @@
         _Atom.prototype.name= ''
 
         _Atom.prototype._getCell= function (x, y) {
-            return __getCell(this._atomId, x, y);
+            return __getCell(this._atomId, x, y)
         }
 
         _Atom.prototype.getCell= function () {
-            var ranges= _flatten(this.getRanges());
-            if ( ranges.length === 0 ) return emptyCell;
-            return __getCell(this._atomId, ranges[0][0], ranges[0][1]);
+            var ranges= _flatten(this.getRanges())
+            if ( ranges.length === 0 ) return emptyCell
+            return __getCell(this._atomId, ranges[0][0], ranges[0][1])
         }
 
         _Atom.prototype.value= function() {
-            var ranges= _flatten(this.getRanges());
-            if ( ranges.length === 0 ) return '#NV';
-            var cell= this._getCell(ranges[0][0], ranges[0][1]);
-            return cell ? cell.value : '#EMPTY';
+            var ranges= _flatten(this.getRanges())
+            if ( ranges.length === 0 ) return '#NV'
+            var cell= this._getCell(ranges[0][0], ranges[0][1])
+            return cell ? cell.value : '#EMPTY'
         }
 
         _Atom.extend= function( name, protoFn, factoryFn ) {
-            _Atom.prototype[name]= factoryFn;
-            protoFn.prototype.name= name;
-            protoFn.prototype.__proto__= _Atom.prototype;
+            _Atom.prototype[name]= factoryFn
+            protoFn.prototype.name= name
+            protoFn.prototype.__proto__= _Atom.prototype
         };
 
 // =============================================================================
@@ -89,23 +109,23 @@
 // =============================================================================
 
         var _Dump= function( parent, comment ) {
-            _Atom.call(this, parent);
+            _Atom.call(this, parent)
 
             var _dump= function ( ranges, comment ) {
-                var out= [];
+                var out= []
                 for each ( var range in ranges ) {
-                    out.push('[' + range[0] + ',' + range[1] + ',' + range[2] + ',' + range[3] + ']');
+                    out.push('[' + range[0] + ',' + range[1] + ',' + range[2] + ',' + range[3] + ']')
                 }
-                console.log((comment ? comment + ': [' : '[') + out.join(', ') + ']');
+                console.log((comment ? comment + ': [' : '[') + out.join(', ') + ']')
             }
 // console.log(parent, ranges,arg);
 
-            _dump(parent.getRanges(), comment );
+            _dump(parent.getRanges(), comment )
 
             this.getRanges= function() parent.getRanges()
         }
 
-        _Atom.extend('dump', _Dump, function( comment ) new _Dump(this, comment));
+        _Atom.extend('dump', _Dump, function( comment ) new _Dump(this, comment))
 
 
 // =============================================================================
@@ -115,45 +135,45 @@
         var __addRange= function( ranges, arg ) {
 
             if ( _isArray(arg) ) {
-                if ( arg.length === 1 ) return __addRange(ranges, arg[0]);
+                if ( arg.length === 1 ) return __addRange(ranges, arg[0])
                 if ( arg.length === 2 ) {
                     if ( typeof arg[0] === 'string' && typeof arg[1] === 'string' ) {
-                        var cellTL= stringToCell(arg[0]);
-                        var cellBR= stringToCell(arg[1]);
-                        ranges.push([ cellTL[0], cellTL[1], cellBR[0], cellBR[1] ]);
+                        var cellTL= stringToCell(arg[0])
+                        var cellBR= stringToCell(arg[1])
+                        ranges.push([ cellTL[0], cellTL[1], cellBR[0], cellBR[1] ])
                         return ranges;
                     }
                     if ( typeof arg[0] === 'number' && typeof arg[1] === 'number' ) {
-                        ranges.push([ arg[0], arg[1], arg[0], arg[1] ]);
-                        return ranges;
+                        ranges.push([ arg[0], arg[1], arg[0], arg[1] ])
+                        return ranges
                     }
                 }
                 if ( arg.length === 4 ) {
-                    ranges.push(arg);
+                    ranges.push(arg)
                     return ranges;
                 }
             }
             else if ( arg instanceof Ranges ) {
-                for each ( var range in arg._ranges ) __addRange(ranges, range);
+                for each ( var range in arg._ranges ) __addRange(ranges, range)
                 return ranges;
             }
 
-            console.log("_addRange error:", arg, Object.prototype.toString.call(arg));
+            console.log("_addRange error:", arg, Object.prototype.toString.call(arg))
 
-            throw "RangeArgException:" + arg;
+            throw "RangeArgException:" + arg
         };
 
         var _AddRange= function( parent, arg ) {
-            _Atom.call(this, parent);
+            _Atom.call(this, parent)
 
 // console.log(parent, ranges,arg);
 
             this.getRanges= function() {
-                return __addRange(parent.getRanges(), arg);
+                return __addRange(parent.getRanges(), arg)
             };
         }
 
-        _Atom.extend('addRange', _AddRange, function() new _AddRange(this, Array.prototype.slice.call(arguments)));
+        _Atom.extend('addRange', _AddRange, function() new _AddRange(this, Array.prototype.slice.call(arguments)))
 
 
 // =============================================================================
@@ -161,16 +181,16 @@
 // =============================================================================
 
         var _AddRanges= function( parent, newRanges ) {
-            _Atom.call(this, parent);
+            _Atom.call(this, parent)
 
             this.getRanges= function() {
-                var _ranges= parent.getRanges();
-                for each (var arg in newRanges) __addRange(ranges, arg);
-                return ranges;
+                var _ranges= parent.getRanges()
+                for each (var arg in newRanges) __addRange(ranges, arg)
+                return ranges
             };
         }
 
-        _Atom.extend('addRanges', _AddRanges, function(newRanges) new _AddRanges(this, newRanges));
+        _Atom.extend('addRanges', _AddRanges, function(newRanges) new _AddRanges(this, newRanges))
 
 
 // =============================================================================
@@ -179,36 +199,36 @@
 
 /*
         var intersection= function () {
-            if ( ranges.length <= 1 ) return this;
-            var newRange= null;
+            if ( ranges.length <= 1 ) return this
+            var newRange= null
             for each ( var range in ranges ) {
                 if ( newRange === null ) {
-                    newRange= range.concat();
+                    newRange= range.concat()
                     continue;
                 }
-                if ( range[0] > newRange[0] ) newRange[0]= range[0];
-                if ( range[1] > newRange[1] ) newRange[1]= range[1];
-                if ( range[2] < newRange[2] ) newRange[2]= range[2];
-                if ( range[3] < newRange[3] ) newRange[3]= range[3];
+                if ( range[0] > newRange[0] ) newRange[0]= range[0]
+                if ( range[1] > newRange[1] ) newRange[1]= range[1]
+                if ( range[2] < newRange[2] ) newRange[2]= range[2]
+                if ( range[3] < newRange[3] ) newRange[3]= range[3]
             }
-            if ( newRange[0] > newRange[2] || newRange[1] > newRange[3] ) return new Ranges([]);
-            return new Ranges([ newRange ]);
+            if ( newRange[0] > newRange[2] || newRange[1] > newRange[3] ) return new Ranges([])
+            return new Ranges([ newRange ])
         };
 
         var union= function () {
             if ( ranges.length <= 1 ) return this;
-            var newRange= null;
+            var newRange= null
             for each ( var range in ranges ) {
                 if ( newRange === null ) {
-                    newRange= range.concat();
-                    continue;
+                    newRange= range.concat()
+                    continue
                 }
-                if ( range[0] < newRange[0] ) newRange[0]= range[0];
-                if ( range[1] < newRange[1] ) newRange[1]= range[1];
-                if ( range[2] > newRange[2] ) newRange[2]= range[2];
-                if ( range[3] > newRange[3] ) newRange[3]= range[3];
+                if ( range[0] < newRange[0] ) newRange[0]= range[0]
+                if ( range[1] < newRange[1] ) newRange[1]= range[1]
+                if ( range[2] > newRange[2] ) newRange[2]= range[2]
+                if ( range[3] > newRange[3] ) newRange[3]= range[3]
             }
-            return new Ranges([ newRange ]);
+            return new Ranges([ newRange ])
         };
 */
 
@@ -218,30 +238,30 @@
 // =============================================================================
 
         var _Crop= function( parent, x0, y0, x1, y1 ) {
-            _Atom.call(this, parent);
+            _Atom.call(this, parent)
 
             // TODO: pruefen auf x0/y0 < 0 ? Oder macht flatten das?
 
-            if ( x1 === undefined ) x1= x0;
-            if ( y1 === undefined ) y1= y0;
+            if ( x1 === undefined ) x1= x0
+            if ( y1 === undefined ) y1= y0
 
             this.getRanges= function() {
-                var ranges= [];
+                var ranges= []
                 for each ( let [ ox0, ox1, oy0, oy1 ] in parent.getRanges() ) {
                     var newRange= [ ox0, oy0, ox1, oy1 ]= [
                         ox0 + x0,
                         oy0 + y0,
                         (ox0 + x1 <= ox1 ? ox0 + x1 : ox1),
                         (oy0 + y1 <= oy1 ? oy0 + y1 : oy1)
-                    ];
-                    if ( ox0 > ox1 || oy0 > oy1 ) continue;
-                    ranges.push(newRange);
+                    ]
+                    if ( ox0 > ox1 || oy0 > oy1 ) continue
+                    ranges.push(newRange)
                 }
-                return ranges;
+                return ranges
             };
         };
 
-        _Atom.extend('crop', _Crop, function(x0, y0, x1, y1) new _Crop(this, x0, y0, x1, y1));
+        _Atom.extend('crop', _Crop, function(x0, y0, x1, y1) new _Crop(this, x0, y0, x1, y1))
 
 
 // =============================================================================
@@ -249,46 +269,46 @@
 // =============================================================================
 
         var _Ofs= function( parent, x, y ) {
-            _Atom.call(this, parent);
+            _Atom.call(this, parent)
 
             this.getRanges= function() {
-                var ranges= [];
+                var ranges= []
                 for each (var range in parent.getRanges()) {
-                    ranges.push([ range[0] + x, range[1] + y, range[2] + x, range[3] + y ]);
+                    ranges.push([ range[0] + x, range[1] + y, range[2] + x, range[3] + y ])
                 }
-                return ranges;
+                return ranges
             };
         };
 
-        _Atom.extend('ofs', _Ofs, function(x, y) new _Ofs(this, x,y ));
+        _Atom.extend('ofs', _Ofs, function(x, y) new _Ofs(this, x,y ))
 
         // TODO: Naiver Code. Optimierte Variante schreiben!
         var _flatten= function ( ranges ) {
 
-            if ( ranges.length === 0 ) return ranges;
+            if ( ranges.length === 0 ) return ranges
 
             // OPTIMIERUNG TESTEN: 
             // if ( ranges.length === 1 && ranges[0][0] === ranges[0][2] && ranges[0][1] === ranges[0][3] ) return this;
 
-            var lookup= {};
+            var lookup= {}
 
             // Irgendwie die Ranges merken:
             // - Key bauen fuer alle Ranges
 
             // Beispiel: SUMME(B:B), dann Aenderung (oder hinzufuegen von B7)
 
-            var newRanges= [];
+            var newRanges= []
             for each ( var range in ranges ) {
                 for ( var y= range[1]; y <= range[3]; y++ ) {
                     for ( var x= range[0]; x <= range[2]; x++ ) {
-                        if ( x < 0 || y < 0 ) continue;
-                        if ( lookup[x + ':' + y] ) continue;
-                        newRanges.push([x, y, x, y]);
-                        lookup[x + ':' + y]= 1;
+                        if ( x < 0 || y < 0 ) continue
+                        if ( lookup[x + ':' + y] ) continue
+                        newRanges.push([x, y, x, y])
+                        lookup[x + ':' + y]= 1
                     }
                 }
             }
-            return newRanges;
+            return newRanges
         }
 
 /*
@@ -329,14 +349,14 @@
 // =============================================================================
 
         var _Flatten= function ( parent ) {
-            _Atom.call(this, parent);
+            _Atom.call(this, parent)
 
             this.getRanges= function() {
-                return _flatten(parent.getRanges());
+                return _flatten(parent.getRanges())
             }
         }
 
-        _Atom.extend('flatten', _Flatten, function() new _Flatten(this));
+        _Atom.extend('flatten', _Flatten, function() new _Flatten(this))
 
 
 // =============================================================================
@@ -344,35 +364,35 @@
 // =============================================================================
 
         var _Grep= function ( parent, value ) {
-            _Atom.call(this, parent);
+            _Atom.call(this, parent)
 
             this.getRanges= function() {
-                var newRanges= [];
+                var newRanges= []
                 for each ( var range in _flatten(parent.getRanges()) ) {
-                    var cell= this._getCell(range[0], range[1]);
+                    var cell= this._getCell(range[0], range[1])
                     if ( cell !== undefined && cell.value == value ) {
-                        newRanges.push(range);
+                        newRanges.push(range)
                     }
                 }
-                return newRanges;
+                return newRanges
             }
         };
 
-        _Atom.extend('grep', _Grep, function(value) new _Grep(this, value));
+        _Atom.extend('grep', _Grep, function(value) new _Grep(this, value))
 
 
 // =============================================================================
 //      Interface (window context)
 // =============================================================================
 
-        Ranges= function() new _Atom;
+        Ranges= function() new _Atom
 
-        R= function() (new _Atom).addRange(Array.prototype.slice.call(arguments)).dump('addRange');
+        R= function() (new _Atom).addRange(Array.prototype.slice.call(arguments)).dump('addRange')
 
         C= function (x, y) (new _Atom).addRange(x, y).getCell()
 
         DumpCells= function () {
-            console.log(cells);
+            console.log(cells)
         }
 
 })();
--- assets/testd.js
+++ assets/testd.js
@@ -1,41 +1,43 @@
 
 var test_ranges= function() {
-    // var ranges= new Ranges().addRange( 'C5', 'D8' );
+    // var ranges= new Ranges().addRange( 'C5', 'D8' )
 
-    var ranges= R('C5','D8');
-    ranges.dump("ranges");
+    var ranges= R('C5','D8')
+    ranges.dump("ranges")
 
-    var newranges= ranges.addRange('C3', 'D4');
-    newranges.dump("newranges");
+    var newranges= ranges.addRange('C3', 'D4')
+    newranges.dump("newranges")
 
-    var Inf= Number.MAX_VALUE;
-    var EmptyRange= new Ranges();
+    var Inf= Number.MAX_VALUE
+    var EmptyRange= new Ranges()
 
-    C(2, 4).value= "A1";
-    C(2, 5).value= "A2";
-    C(2, 6).value= "Steppi";
-    C(4, 6).value= "HUHU!";
-    C(2, 7).value= "A4";
+    C(2, 4).value= "C5"
+    C(2, 5).value= "C6"
+    C(2, 6).value= "Steppi"
+    C(4, 6).value= "HUHU!"
+    C(2, 7).value= "C8"
 
-    DumpCells();
+    DumpCells()
 
     function SVERWEIS(searchRanges, value, col_i) {
-        var sv= searchRanges.dump("init").crop(0, 0, 0, Inf).dump("cropped").grep(value);
-        // sv= sv.grep(value);          // geht???
-        // sv= sv.dump("grepped");      // geht nicht???
-        return sv;
+        // var sv= searchRanges.dump("init").crop(0, 0, 0, Inf).dump("cropped").grep(value).ofs(col_i, 0)
+        // sv= sv.grep(value)          // geht???
+        // sv= sv.dump("grepped")      // geht nicht??? oder jetzt doch? hmm
+        // return sv
 
-        return searchRanges.crop(0, 0, 0, Inf).grep(value).ofs(col_i, 0);
+        // ofs ist evt falsch, wenn sich col_i innerhalb von searchRanges befinden muss
+
+        return searchRanges.crop(0, 0, 0, Inf).grep(value).ofs(col_i, 0)
     }
 
-    var sv= SVERWEIS(ranges, "Steppi", 2) .dump("sverweis Steppi");
+    var sv= SVERWEIS(newranges, "Steppi", 2) .dump("sverweis Steppi")
 
-    console.log("Ergebnis:", sv.value() );
+    console.log("Ergebnis:", sv.value() )
 
+    console.log("valueDeps:", sv.valueDeps())
 
-
-    // console.log("Ergebnis:", SVERWEIS(ranges, "Steppi", 2) .dump("sverweis Steppi") .value() );
-    // console.log("Ergebnis:", SVERWEIS(ranges, "Steppi2", 2).dump("sverweis Steppi2").value() );
+    // console.log("Ergebnis:", SVERWEIS(ranges, "Steppi", 2) .dump("sverweis Steppi") .value() )
+    // console.log("Ergebnis:", SVERWEIS(ranges, "Steppi2", 2).dump("sverweis Steppi2").value() )
 
 }
 
@@ -48,11 +50,11 @@ var test_model= function() {
             this.left=      function(n)     new _Left(this, inx, n)
             this.grep=      function(value) new _Grep(this, inx, value)
 
-            this.chainDeps= function() parent ? parent.chainDeps() : inx;
-            this.valueDeps= function() parent ? parent.valueDeps() : inx;
+            this.chainDeps= function() parent ? parent.chainDeps() : inx
+            this.valueDeps= function() parent ? parent.valueDeps() : inx
     
             this.value= function() {
-                return inx.map(function (inxv) { return vals[inxv]; })
+                return inx.map(function (inxv) { return vals[inxv] })
             }
 
             this.__inx= function() inx      // DEBUG
@@ -61,28 +63,28 @@ var test_model= function() {
         var union= function(ar1, ar2) ar1.concat(ar2.filter( function(v) ar1.indexOf(v) < 0 )) 
 
         var _Left= function (parent, inx, n) {
-            inx= inx.slice(0, n);
-            _List.call(this, parent, inx);
+            inx= inx.slice(0, n)
+            _List.call(this, parent, inx)
             this.valueDeps= function() inx
         }
 
         var _Add= function (parent, inx, inx1) {
-            inx= union(inx, inx1);
-            _List.call(this, parent, inx);
-            delete inx1;
-            this.chainDeps= function() parent ? union(parent.chainDeps(), inx1) : inx;
+            inx= union(inx, inx1)
+            _List.call(this, parent, inx)
+            delete inx1
+            this.chainDeps= function() parent ? union(parent.chainDeps(), inx1) : inx
             this.valueDeps= function() inx
         }
 
         var _Grep= function (parent, inx, value) {
-            inx= inx.filter(function (inxv) { return value == vals[inxv]; });
-            _List.call(this, parent, inx);
+            inx= inx.filter(function (inxv) { return value == vals[inxv] })
+            _List.call(this, parent, inx)
             this.valueDeps= function() inx
         }
 
-        _Add.prototype.__proto__= _List.prototype;
+        _Add.prototype.__proto__= _List.prototype
 
-        return new _List(null, []);
+        return new _List(null, [])
     }
 
     var cell= List([ 'A', 'B', 'B', 'D', 'E' ])
@@ -92,21 +94,22 @@ var test_model= function() {
         .left(3)
         .grep('B')
         .add([ 4 ])
-    ;
+    
 
-    console.log(cell);
-    console.log(cell.__inx());
-    console.log("RECALC IN CHANGES IN:", cell.chainDeps());
-    console.log("CIRCULAR REFS IF:", cell.valueDeps());
-    console.log("RESULT:", cell.value());
+    console.log(cell)
+    console.log(cell.__inx())
+    console.log("RECALC IN CHANGES IN:", cell.chainDeps())
+    console.log("CIRCULAR REFS IF:", cell.valueDeps())
+    console.log("RESULT:", cell.value())
 }
 
 var test_let= function() {
-    let x= 1;
-    var ar= [ [ 1, 2], [3, 4], [5, 6] ];
-    for each ( let [x, y] in ar ) console.log("let test:", x, y);
+    let x= 1
+    var ar= [ [ 1, 2], [3, 4], [5, 6] ]
+    for each ( let [x, y] in ar ) console.log("let test:", x, y)
 }
 
-test_ranges();
-// test_model();
-// test_let();
+test_ranges()
+// test_model()
+// test_let()
+
