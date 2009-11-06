b8b030de4aeb8e4d0ab14d0d7c0451d0f6457ede
steppicrew <rabak@steppicrew.de>
2009-11-06 09:54:56 +0100

    console's log functioniality is modified to support log levels

--- assets/common.js
+++ assets/common.js
@@ -1,2 +1,39 @@
 
-if ( typeof console === 'undefined' ) console= { log: function () {}, warn: function () {}, error: function () {} , debug: function () {} }
+// modify firebug's console to implement log level
+// new method "console.level()" gets/sets log level
+// console.debug/info/warn/error are called only if log level is higher or equal
+
+(function() {
+    var _level
+    var _levelMap= {
+        debug: 7,
+        log: 6,
+        info: 5,
+        warn: 4,
+        error: 3,
+    }
+
+    console.level= function(level) {
+        if (typeof level !== 'undefined') {
+            _level= _levelMap[level] || level
+        }
+        return _level
+    }
+
+    console.level('debug')
+
+    var Log= function(level, fn, args) {
+        if (level <= _level) fn.call(firefoxConsole, args)
+    }
+
+    for (var level in _levelMap) {
+        console[level]= (function(level) {
+            var origFn= console[level] || function() {}
+            level= _levelMap[level]
+            return function() {
+                if (level <= _level) origFn.apply(console, arguments)
+            }
+        })(level)
+    }
+
+})()
