ee88d0ac6c001c1cad5eb0d66935d4e7c1305919
steppicrew <rabak@steppicrew.de>
2009-11-23 00:22:56 +0100

    name functions for better debugging make lists consist of constructors and strings (instead of GE-objects) to prevent recursion

--- assets/grammar-parser.js
+++ assets/grammar-parser.js
@@ -8,7 +8,7 @@
         fnProto.prototype.__super__= fnClass
     }
 
-    var GEInstance= function(text, match, ge, data) {
+    var GEInstance= function GEInstance(text, match, ge, data) {
         this.match=   match
         this.text=    text
         this.remain=  text.substr(match.length)
@@ -16,9 +16,10 @@
 
         this.data= data
         this.ge= ge
+        this.geName= ge.constructor.name
     }
 
-    var GE= function() {}
+    var GE= function GE() {}
     GE.prototype.match= function(_in) {
         var result= this._match(_in)
         if (result === undefined) return // undefined
@@ -32,7 +33,7 @@
     GE.prototype._match= function() { throw new Error("This method should be overwritten.") }
     GE.prototype.process= function() true
 
-    var GE_Literal= function(literal) {
+    var GE_Literal= function GE_Literal(literal) {
         GE_Literal.prototype.__super__.call(this)
         this._match= function(_in) {
             if (_in.indexOf(literal) === 0) return {match: literal}
@@ -41,11 +42,14 @@
     _extend(GE, GE_Literal)
 
     // defines a list of alternative grammar
-    var GE_ListOr= function(elements) {
+    var GE_ListOr= function GE_ListOr(elements) {
         GE_ListOr.prototype.__super__.call(this)
 
         this._match= function(_in) {
-            for each (let element in elements) {
+            for each (let fnElement in elements) {
+                let element= typeof fnElement === 'string'
+                    ? new GE_Literal(fnElement)
+                    : new fnElement
                 let result= element.match(_in)
                 if (result === undefined) continue
                 return {
@@ -59,13 +63,16 @@
     _extend(GE, GE_ListOr)
 
     // defines a grammar list
-    var GE_List= function(elements) {
+    var GE_List= function GE_List(elements) {
         GE_List.prototype.__super__.call(this)
 
         this._match= function(_in) {
             var matches= []
             var match= ""
-            for each (let element in elements) {
+            for each (let fnElement in elements) {
+                let element= typeof fnElement === 'string'
+                    ? new GE_Literal(fnElement)
+                    : new fnElement
                 let result= element.match(_in)
                 if (result === undefined) return // undefined
                 matches.push(result)
@@ -81,7 +88,7 @@
     GE_List.prototype.process= function() { throw new Error("List's processor should be overwritten") }
     _extend(GE, GE_List)
 
-    var GE_Int= function() GE_Int.prototype.__super__.call(this)
+    var GE_Int= function GE_Int() GE_Int.prototype.__super__.call(this)
     GE_Int.prototype._match= function(_in) {
         var match= _in.match(/^\-?\d+/)
         if (match === null) return // undefined
@@ -93,7 +100,7 @@
     GE_Int.prototype.process= function(data) data
     _extend(GE, GE_Int)
 
-    var GE_Identifier= function() GE_Identifier.prototype.__super__.call(this)
+    var GE_Identifier= function GE_Identifier() GE_Identifier.prototype.__super__.call(this)
     GE_Identifier.prototype._match= function(_in) {
         var match= _in.match(/^[a-z_]\w*/i)
         if (match === null) return // undefined
@@ -101,43 +108,43 @@
     }
     _extend(GE, GE_Identifier)
 
-    var GE_Variable= function() {
+    var GE_Variable= function GE_Variable() {
         GE_Variable.prototype.__super__.call(this, [
-            new GE_Literal('$'),
-            new GE_Identifier(),
+            '$',
+            GE_Identifier,
         ])
     }
     _extend(GE_List, GE_Variable)
 
-    var GE_FunctionCall= function() {
+    var GE_FunctionCall= function GE_FunctionCall() {
         GE_FunctionCall.prototype.__super__.call(this, [
-            new GE_Identifier(),
-            new GE_Literal('('),
-            new GE_Statement(),
-            new GE_Literal(')'),
+            GE_Identifier,
+            '(',
+            GE_Statement,
+            ')',
         ])
     }
     _extend(GE_List, GE_FunctionCall)
 
-    var GE_Statement= function() {
+    var GE_Statement= function GE_Statement() {
         GE_Statement.prototype.__super__.call(this, [
-            new GE_Variable(),
-            new GE_Int(),
-            new GE_FunctionCall(),
+            GE_Variable,
+            GE_Int,
+            GE_FunctionCall,
         ])
     }
     _extend(GE_ListOr, GE_Statement)
 
-    var GE_Assignment= function() {
+    var GE_Assignment= function GE_Assignment() {
         GE_Assignment.prototype.__super__.call(this, [
-            new GE_Variable(),
-            new GE_Literal('='),
-            new GE_Statement(),
+            GE_Variable,
+            '=',
+            GE_Statement,
         ])
     }
     _extend(GE_List, GE_Assignment)
 
-    var testString="$huhu=x78"
+    var testString="$huhu=zuppi(78)"
     var test= new GE_Assignment
     console.log(test.match(testString))
 
